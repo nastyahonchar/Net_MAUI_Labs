1adaab04652fee9b9da8f02b56bfa2356b763eb7
Anastasia Honchar <honchar.anastasiia@chnu.edu.ua>
2026-05-10 18:44:46 +0300

    Add project files.

---
+++ Phoneword/Phoneword/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Phoneword")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Phoneword.Pages")]
